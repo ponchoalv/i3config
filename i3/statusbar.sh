5083c89fce177354da1eaf6766043089a40eeb57
poncho <poncho@localhost.localdomain>
2018-06-16 00:12:19 -0300

    Agregué iconos para la batería, scrips para audio hdmi y para cambiar la proyeccion de pantallas adicionales

--- i3/statusbar.sh
+++ i3/statusbar.sh
@@ -2,14 +2,17 @@
 /usr/bin/i3status | (read line && echo $line && read line && echo $line && while :
 do
   read line
+  monitor_mode=`cat /tmp/monitor_mode.dat`
+  monitor_json="{\"name\":\"monitors_mode\",\"full_text\":\"$monitor_mode\"}"
+
   id=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
   if [ x$id != x ]; then
     name=$(xprop -id $id | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
     name=${name//\\/\\\\}
     name=${name//\"/\\\"}
     dat="[{\"name\":\"title\",\"full_text\":\"$name\"},"
-    echo "${line/[/$dat}" || exit 1
+    echo "${line/[/$dat$monitor_json,}" || exit 1
   else
-    echo "$line" || exit 1
+    echo "${line/[/$monitor_json}" || exit 1
   fi
 done)
